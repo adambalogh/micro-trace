9030e451629e2818d0feeb223e811d928d4229af
Adam Ubuntu <balogh.adam@icloud.com>
2017-04-27 14:51:03 +0100

    more syscall

--- recv.c
+++ recv.c
@@ -14,6 +14,8 @@ typedef ssize_t (*orig_read_t)(int fd, void *buf, size_t count);
 typedef ssize_t (*orig_recvfrom_t)(int sockfd, void *buf, size_t len, int flags,
                                    struct sockaddr *src_addr, socklen_t *addrlen);
 typedef ssize_t (*orig_send_t)(int sockfd, const void *buf, size_t len, int flags);
+typedef int (*orig_accept_t)(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+
 
 __thread int initiator_socket = -1;
 
@@ -21,15 +23,27 @@ void* orig(const char* name) {
     return dlsym(RTLD_NEXT, name);
 }
 
+int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen) {
+    orig_accept_t orig_accept = (orig_accept_t) orig("accept");
+    int ret = orig_accept(sockfd, addr, addrlen);
+    if (ret != -1) {
+        printf("got new connection: %d\n", ret);
+        if (initiator_socket == -1) {
+            initiator_socket = ret;
+        }
+    }
+    return ret;
+}
+
 ssize_t send(int sockfd, const void *buf, size_t len, int flags) {
-    printf("send\n");
+    printf("%d: send %d\n", initiator_socket, sockfd);
     orig_send_t orig_send = (orig_send_t) orig("send");
     return orig_send(sockfd, buf, len, flags);
 }
 
 ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags,
                  struct sockaddr *src_addr, socklen_t *addrlen) {
-    printf("recvfrom\n");
+    printf("%d: recvfrom %d\n", initiator_socket, sockfd);
     orig_recvfrom_t orig_recvfrom = (orig_recvfrom_t) orig("recvfrom");
     return orig_recvfrom(sockfd, buf, len, flags, src_addr, addrlen);
 }
@@ -37,7 +51,7 @@ ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags,
 int socket(int domain, int type, int protocol) {
     orig_socket_t orig_socket = (orig_socket_t) orig("socket");
     int ret = orig_socket(domain, type, protocol);
-    printf("socket: %d\n", ret);
+    printf("%d: socket: %d\n", initiator_socket, ret);
     return ret;
 }
 
