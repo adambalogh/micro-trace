e9ea1766b58e1a80832bca3ce531af7d01aa1132
Adam Ubuntu <balogh.adam@icloud.com>
2017-04-27 16:08:58 +0100

    changed logging

--- recv.c
+++ recv.c
@@ -1,18 +1,19 @@
 #define _GNU_SOURCE
 
+#include <errno.h>
 #include <dlfcn.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/epoll.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <time.h>
 
 #include "uthash.h"
 
 typedef int (*orig_epoll_wait_t)(int epfd, struct epoll_event *events,
                                  int maxevents, int timeout);
 typedef int (*orig_socket_t)(int domain, int type, int protocol);
-typedef ssize_t (*orig_read_t)(int fd, void *buf, size_t count);
 typedef ssize_t (*orig_recvfrom_t)(int sockfd, void *buf, size_t len, int flags,
                                    struct sockaddr *src_addr, socklen_t *addrlen);
 typedef ssize_t (*orig_send_t)(int sockfd, const void *buf, size_t len, int flags);
@@ -35,12 +36,12 @@ void add_parent(const int sockfd, const int parent_sockfd) {
     socket_entry_t* entry = (socket_entry_t*) malloc(sizeof(socket_entry_t));
     entry->fd = sockfd;
     entry->parent_fd = parent_sockfd;
-    HASH_ADD_INT(sockets, fd, entry);  /* id: name of key field */
+    HASH_ADD_INT(sockets, fd, entry);
 }
 
 int get_parent(const int sockfd) {
     const socket_entry_t* entry;
-    HASH_FIND_INT(sockets, &sockfd, entry);  /* s: output pointer */
+    HASH_FIND_INT(sockets, &sockfd, entry);
     return entry->parent_fd;
 }
 
@@ -48,24 +49,24 @@ int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen) {
     orig_accept_t orig_accept = (orig_accept_t) orig("accept");
     int ret = orig_accept(sockfd, addr, addrlen);
     if (ret != -1) {
-        printf("got new connection: %d\n", ret);
+        time_t t = time(NULL);
+        printf("@%ld: accepted socket: %d\n", t, ret);
         add_parent(ret, ret);
-        if (current_socket == -1) {
-            current_socket = ret;
-        }
+        current_socket = ret;
     }
     return ret;
 }
 
 ssize_t send(int sockfd, const void *buf, size_t len, int flags) {
-    printf("%d: send %d\n", get_parent(sockfd), sockfd);
     orig_send_t orig_send = (orig_send_t) orig("send");
-    return orig_send(sockfd, buf, len, flags);
+    ssize_t ret = orig_send(sockfd, buf, len, flags);
+    printf("%d: sent %ld\n", get_parent(sockfd), ret);
+    return ret;
 }
 
 ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags,
                  struct sockaddr *src_addr, socklen_t *addrlen) {
-    printf("%d: recvfrom %d\n", get_parent(sockfd), sockfd);
+    current_socket = get_parent(sockfd);
     orig_recvfrom_t orig_recvfrom = (orig_recvfrom_t) orig("recvfrom");
     return orig_recvfrom(sockfd, buf, len, flags, src_addr, addrlen);
 }
@@ -73,15 +74,18 @@ ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags,
 int socket(int domain, int type, int protocol) {
     orig_socket_t orig_socket = (orig_socket_t) orig("socket");
     int ret = orig_socket(domain, type, protocol);
-
     add_parent(ret, current_socket);
-    printf("%d: socket: %d\n", current_socket, ret);
-    
+    /*printf("%d: socket: %d\n", current_socket, ret);*/
     return ret;
 }
 
 int epoll_wait(int epfd, struct epoll_event *events,
                int maxevents, int timeout) {
 	orig_epoll_wait_t orig_epoll = (orig_epoll_wait_t) orig("epoll_wait");
-	return orig_epoll(epfd, events, maxevents, timeout);
+	int ret = orig_epoll(epfd, events, maxevents, timeout);
+    if (ret != 0) {
+        /*printf("epoll_wait: %d ready\n", ret);*/
+    }
+
+    return ret;
 }
