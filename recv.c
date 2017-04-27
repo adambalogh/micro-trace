5b1b718fea05576a20957da49ee74bd7ad8e75ad
Adam Ubuntu <balogh.adam@icloud.com>
2017-04-27 15:12:42 +0100

    store parent of each sockt

--- recv.c
+++ recv.c
@@ -7,6 +7,8 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
+#include "uthash.h"
+
 typedef int (*orig_epoll_wait_t)(int epfd, struct epoll_event *events,
                                  int maxevents, int timeout);
 typedef int (*orig_socket_t)(int domain, int type, int protocol);
@@ -16,11 +18,30 @@ typedef ssize_t (*orig_recvfrom_t)(int sockfd, void *buf, size_t len, int flags,
 typedef ssize_t (*orig_send_t)(int sockfd, const void *buf, size_t len, int flags);
 typedef int (*orig_accept_t)(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
 
+typedef struct {
+    int fd;
+    int parent_fd;
+    UT_hash_handle hh;         /* makes this structure hashable */
+} socket_entry_t;
 
-__thread int initiator_socket = -1;
+__thread int current_socket = -1;
+__thread socket_entry_t* sockets = NULL;
 
 void* orig(const char* name) {
     return dlsym(RTLD_NEXT, name);
+}
+
+void add_parent(const int sockfd, const int parent_sockfd) {
+    socket_entry_t* entry = (socket_entry_t*) malloc(sizeof(socket_entry_t));
+    entry->fd = sockfd;
+    entry->parent_fd = parent_sockfd;
+    HASH_ADD_INT(sockets, fd, entry);  /* id: name of key field */
+}
+
+int get_parent(const int sockfd) {
+    const socket_entry_t* entry;
+    HASH_FIND_INT(sockets, &sockfd, entry);  /* s: output pointer */
+    return entry->parent_fd;
 }
 
 int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen) {
@@ -28,22 +49,23 @@ int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen) {
     int ret = orig_accept(sockfd, addr, addrlen);
     if (ret != -1) {
         printf("got new connection: %d\n", ret);
-        if (initiator_socket == -1) {
-            initiator_socket = ret;
+        add_parent(ret, ret);
+        if (current_socket == -1) {
+            current_socket = ret;
         }
     }
     return ret;
 }
 
 ssize_t send(int sockfd, const void *buf, size_t len, int flags) {
-    printf("%d: send %d\n", initiator_socket, sockfd);
+    printf("%d: send %d\n", get_parent(sockfd), sockfd);
     orig_send_t orig_send = (orig_send_t) orig("send");
     return orig_send(sockfd, buf, len, flags);
 }
 
 ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags,
                  struct sockaddr *src_addr, socklen_t *addrlen) {
-    printf("%d: recvfrom %d\n", initiator_socket, sockfd);
+    printf("%d: recvfrom %d\n", get_parent(sockfd), sockfd);
     orig_recvfrom_t orig_recvfrom = (orig_recvfrom_t) orig("recvfrom");
     return orig_recvfrom(sockfd, buf, len, flags, src_addr, addrlen);
 }
@@ -51,7 +73,10 @@ ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags,
 int socket(int domain, int type, int protocol) {
     orig_socket_t orig_socket = (orig_socket_t) orig("socket");
     int ret = orig_socket(domain, type, protocol);
-    printf("%d: socket: %d\n", initiator_socket, ret);
+
+    add_parent(ret, current_socket);
+    printf("%d: socket: %d\n", current_socket, ret);
+    
     return ret;
 }
 
