7befa00699d7c4416a1f068f6184c5572977db3f
Adam Ubuntu <balogh.adam@icloud.com>
2017-05-09 16:49:30 +0100

    new message tracing

--- tracing_socket.cc
+++ tracing_socket.cc
@@ -68,6 +68,8 @@ int TracingSocket::SetConnid() {
     return 0;
 }
 
+void TracingSocket::BeforeRead() {}
+
 void TracingSocket::AfterRead(const void *buf, size_t len) {
     // Set connid if it hasn't been set before, e.g. in case of
     // when a socket was opened using connect().
@@ -77,6 +79,16 @@ void TracingSocket::AfterRead(const void *buf, size_t len) {
 
     set_current_trace(trace());
     DLOG("%d received %ld bytes", fd(), ret);
+
+    state_ = SocketState::READ;
+}
+
+void TracingSocket::BeforeWrite() {
+    if (!role_client()) return;
+
+    if (state_ == SocketState::WILL_WRITE || state_ == SocketState::READ) {
+        printf("writing start of new message\n");
+    }
 }
 
 void TracingSocket::AfterWrite(ssize_t len) {
@@ -95,6 +107,8 @@ void TracingSocket::AfterWrite(ssize_t len) {
             LOG("sent %ld bytes", len);
         }
     }
+
+    state_ = SocketState::WRITE;
 }
 
 ssize_t TracingSocket::RecvFrom(void *buf, size_t len, int flags,
@@ -127,18 +141,21 @@ ssize_t TracingSocket::Read(void *buf, size_t count) {
 }
 
 ssize_t TracingSocket::Send(const void *buf, size_t len, int flags) {
+    BeforeWrite();
     ssize_t ret = orig.orig_send(fd(), buf, len, flags);
     AfterWrite(ret);
     return ret;
 }
 
 ssize_t TracingSocket::Write(const void *buf, size_t count) {
+    BeforeWrite();
     ssize_t ret = orig.orig_write(fd(), buf, count);
     AfterWrite(ret);
     return ret;
 }
 
 ssize_t TracingSocket::Writev(const struct iovec *iov, int iovcnt) {
+    BeforeWrite();
     ssize_t ret = orig.orig_writev(fd(), iov, iovcnt);
     AfterWrite(ret);
     return ret;
