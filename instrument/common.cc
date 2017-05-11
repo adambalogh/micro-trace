613b133ea0301c6bbe7ad16fa0116f131531cc47
Adam Ubuntu <balogh.adam@icloud.com>
2017-05-11 23:06:17 +0100

    current_trace made static

--- instrument/common.cc
+++ instrument/common.cc
@@ -1,3 +1,11 @@
 #include "common.h"
 
-thread_local trace_id_t __current_trace = UNDEFINED_TRACE;
+static thread_local trace_id_t current_trace = UNDEFINED_TRACE;
+
+trace_id_t get_current_trace() { return current_trace; }
+
+void set_current_trace(const trace_id_t trace) {
+    if (valid_trace(trace)) {
+        current_trace = trace;
+    }
+}
--- instrument/common.h
+++ instrument/common.h
@@ -3,11 +3,13 @@
 #include <assert.h>
 #include <string>
 
-#define LOG(msg, ...)                                \
-    pthread_t thread = pthread_self();               \
-    printf("[t:%lu %d]: ", thread, __current_trace); \
-    printf(msg, __VA_ARGS__);                        \
+#define LOG(msg, ...)                                    \
+    pthread_t thread = pthread_self();                   \
+    printf("[t:%lu %d]: ", thread, get_current_trace()); \
+    printf(msg, __VA_ARGS__);                            \
     printf("\n");
+
+#define DEBUG
 
 #ifdef DEBUG
 #define DLOG(msg, ...) LOG(msg, __VA_ARGS__)
@@ -21,16 +23,10 @@ inline char* string_arr(std::string& str) { return &str[0]; }
 
 typedef int32_t trace_id_t;
 
-extern thread_local trace_id_t __current_trace;
-
 inline int valid_trace(const trace_id_t trace) {
     return trace != UNDEFINED_TRACE && trace != -1;
 }
 
-inline trace_id_t get_current_trace() { return __current_trace; }
+trace_id_t get_current_trace();
 
-inline void set_current_trace(const trace_id_t trace) {
-    if (valid_trace(trace)) {
-        __current_trace = trace;
-    }
-}
+void set_current_trace(const trace_id_t trace);
