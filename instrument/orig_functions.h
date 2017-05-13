4cc7e34f785654970e0a65973b7611671373495d
Adam Ubuntu <balogh.adam@icloud.com>
2017-05-13 15:01:05 +0100

    format

--- instrument/orig_functions.h
+++ instrument/orig_functions.h
@@ -8,18 +8,15 @@
 
 typedef int (*orig_socket_t)(int domain, int type, int protocol);
 typedef int (*orig_close_t)(int fd);
-
 typedef int (*orig_accept_t)(int sockfd, struct sockaddr *addr,
                              socklen_t *addrlen);
 typedef int (*orig_accept4_t)(int sockfd, struct sockaddr *addr,
                               socklen_t *addrlen, int flags);
-
 typedef ssize_t (*orig_recv_t)(int sockfd, void *buf, size_t len, int flags);
 typedef ssize_t (*orig_read_t)(int fd, void *buf, size_t count);
 typedef ssize_t (*orig_recvfrom_t)(int sockfd, void *buf, size_t len, int flags,
                                    struct sockaddr *src_addr,
                                    socklen_t *addrlen);
-
 typedef ssize_t (*orig_write_t)(int fd, const void *buf, size_t count);
 typedef ssize_t (*orig_writev_t)(int fd, const struct iovec *iov, int iovcnt);
 typedef ssize_t (*orig_send_t)(int sockfd, const void *buf, size_t len,
