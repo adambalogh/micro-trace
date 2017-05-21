a6378f39fa25b32360df1035b5aae708a6174a42
Adam Ubuntu <balogh.adam@icloud.com>
2017-05-21 14:41:03 +0100

    mv log to common

--- instrument/trace.h
+++ instrument/trace.h
@@ -5,18 +5,6 @@
 
 #include <boost/uuid/uuid.hpp>
 
-#define LOG(msg, ...)                                    \
-    pthread_t thread = pthread_self();                   \
-    printf("[t:%lu %d]: ", thread, get_current_trace()); \
-    printf(msg, __VA_ARGS__);                            \
-    printf("\n");
-
-#ifdef DEBUG
-#define DLOG(msg, ...) LOG(msg, __VA_ARGS__)
-#else
-#define DLOG(msg, ...)
-#endif
-
 /*
  * This module is responsible for keeping track of the trace associated with the
  * current thread of execution. By default, the trace is undefined.
@@ -31,7 +19,7 @@ typedef boost::uuids::uuid trace_id_t;
 trace_id_t get_current_trace();
 
 /*
- * Sets the trace for the calling thread.
+ * Sets the current trace.
  */
 void set_current_trace(const trace_id_t trace);
 
