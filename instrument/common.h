ed0bc7911392efcf3064ace6adbca06b8a8b133b
Adam Ubuntu <balogh.adam@icloud.com>
2017-05-21 22:24:31 +0100

    rm custom logging

--- instrument/common.h
+++ instrument/common.h
@@ -2,16 +2,4 @@
 
 #include <string>
 
-#define LOG(msg, ...)                                    \
-    pthread_t thread = pthread_self();                   \
-    printf("[t:%lu %d]: ", thread, get_current_trace()); \
-    printf(msg, __VA_ARGS__);                            \
-    printf("\n");
-
-#ifdef DEBUG
-#define DLOG(msg, ...) LOG(msg, __VA_ARGS__)
-#else
-#define DLOG(msg, ...)
-#endif
-
 inline char* string_arr(std::string& str) { return &str[0]; }
