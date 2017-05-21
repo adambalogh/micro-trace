63b71cf685748b18ce5c3f3321c9860e22a37d72
Adam Ubuntu <balogh.adam@icloud.com>
2017-05-21 15:57:01 +0100

    current trace test

--- instrument/test/trace_test.cc
+++ instrument/test/trace_test.cc
@@ -1,5 +1,9 @@
 #include <gtest/gtest.h>
 
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+
 #include "trace.h"
 
 TEST(Trace, Unique) {
@@ -12,11 +16,54 @@ TEST(Trace, Unique) {
 }
 
 TEST(Trace, CurrentTrace) {
-    EXPECT_TRUE(is_trace_undefined());
+    std::thread t1{[]() {
+        EXPECT_TRUE(is_trace_undefined());
 
-    trace_id_t trace = new_trace();
-    set_current_trace(trace);
+        trace_id_t trace = new_trace();
+        set_current_trace(trace);
 
-    EXPECT_FALSE(is_trace_undefined());
-    EXPECT_EQ(trace, get_current_trace());
+        EXPECT_FALSE(is_trace_undefined());
+        EXPECT_EQ(trace, get_current_trace());
+    }};
+    t1.join();
+}
+
+TEST(Trace, UniquePerThread) {
+    std::mutex mu;
+    std::condition_variable cv;
+
+    bool first_trace_set = false;
+    trace_id_t second;
+
+    std::thread t1{[&]() {
+        EXPECT_TRUE(is_trace_undefined());
+
+        {
+            std::unique_lock<std::mutex> l(mu);
+            cv.wait(l, [&first_trace_set]() { return first_trace_set; });
+        }
+
+        EXPECT_TRUE(is_trace_undefined());
+        second = new_trace();
+        set_current_trace(second);
+    }};
+
+    std::thread t2{[&]() {
+        EXPECT_TRUE(is_trace_undefined());
+
+        trace_id_t first = new_trace();
+        set_current_trace(first);
+
+        {
+            std::unique_lock<std::mutex> l(mu);
+            first_trace_set = true;
+        }
+        cv.notify_all();
+        t1.join();
+
+        EXPECT_NE(get_current_trace(), second);
+        EXPECT_EQ(first, get_current_trace());
+    }};
+
+    t2.join();
 }
