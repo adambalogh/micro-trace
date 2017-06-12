ca1eb6c9fadbe5588527855d198075571804cda2
Adam Ubuntu <balogh.adam@icloud.com>
2017-06-12 21:55:17 +0100

    install script

---
+++ instrument/install.sh
@@ -0,0 +1,32 @@
+apt-get install software-properties-common python-software-properties
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+
+apt-get update
+apt-get install gcc-5 g++-5 -y
+apt-get install automake make wget curl tar -y
+
+mkdir /usr/local/download
+cd /usr/local/download
+
+wget https://github.com/gabime/spdlog/archive/v0.13.0.tar.gz -O spdlog.tar.gz
+mkdir spdlog
+tar -xzf spdlog.tar.gz -C spdlog --strip-components=1
+cd spdlog/include
+cp -r spdlog /usr/local/include/.
+
+cd /usr/local/download
+
+wget -c 'http://sourceforge.net/projects/boost/files/boost/1.64.0/boost_1_64_0.tar.gz/download' \
+    -O boost.tar.gz
+mkdir boost
+tar -xzf boost.tar.gz -C boost --strip-components=1
+cd boost
+cp -r boost /usr/local/include/.
+
+cd /usr/local/download
+
+wget https://github.com/libuv/libuv/archive/v1.11.0.tar.gz -O libuv.tar.gz
+mkdir libuv
+tar -xzf libuv.tar.gz -C libuv --strip-components=1
+cd libuv/include
+cp * /usr/local/include/.
