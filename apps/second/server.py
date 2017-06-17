eb2b4d235fd7356cbea1e680f929816956d5fb94
Adam Ubuntu <balogh.adam@icloud.com>
2017-06-17 12:13:36 +0100

    python test app uses connection pool

--- apps/second/server.py
+++ apps/second/server.py
@@ -1,4 +1,5 @@
 import cherrypy
+import urllib3
 import requests
 
 cherrypy.config.update({
@@ -6,12 +7,15 @@ cherrypy.config.update({
     'server.socket_host': '0.0.0.0'
 })
 
+THIRD_URL = 'http://third:3131'
+
+http = urllib3.PoolManager()
 
 class HelloWorld(object):
     @cherrypy.expose
     def index(self):
-        r = requests.get('http://third:3131')
-        return r.text
+        r = http.request('GET', THIRD_URL)
+        return r.data
 
 if __name__ == '__main__':
     cherrypy.quickstart(HelloWorld())
