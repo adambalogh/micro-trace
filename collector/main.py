e271279704598cb2a41f0b3560263b9ecf31cc20
Adam Ubuntu <balogh.adam@icloud.com>
2017-07-25 17:37:05 +0100

    thrift tornado py server

--- collector/main.py
+++ collector/main.py
@@ -8,6 +8,10 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 from thrift.server import TServer
+from thrift import TTornado
+
+from tornado import ioloop
+from tornado.netutil import bind_unix_socket
 
 UNIX_SOCKET = '/tmp/microtrace.thrift'
 
@@ -18,12 +22,14 @@ class CollectorHandler:
 if __name__ == '__main__':
     handler = CollectorHandler()
     processor = Collector.Processor(handler)
-    transport = TSocket.TServerSocket(unix_socket=UNIX_SOCKET)
-    tfactory = TTransport.TBufferedTransportFactory()
     pfactory = TBinaryProtocol.TBinaryProtocolFactory()
-    server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
+    server = TTornado.TTornadoServer(processor, pfactory)
 
     print('Starting the server...')
+    socket = bind_unix_socket(UNIX_SOCKET)
+    server.add_socket(socket)
+    server.start(1)
+    ioloop.IOLoop.instance().start()
     server.serve()
     print('done.')
 
