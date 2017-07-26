49c835b164ddd2534220bf8593018b863a19631b
Adam Ubuntu <balogh.adam@icloud.com>
2017-07-26 19:12:16 +0100

    server logs

--- collector/server.js
+++ collector/server.js
@@ -9,6 +9,7 @@ const SQL_INSERT = 'INSERT INTO spans(body) VALUES($1)';
 
 var server = thrift.createServer(Collector, {
     Collect: function(logs) {
+        console.log('Collecting logs', logs.length);
         for (var i = 0; i < logs.length; ++i) {
             pool.query(SQL_INSERT, [logs[i]], (err, res) => {
                 if (err) {
