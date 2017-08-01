ac416ccd3475c1a9704958d5e401625e02eb6832
Adam Balogh <balogh.adam@icloud.com>
2017-08-01 15:23:49 +0200

    ui improvement

--- ui/server.js
+++ ui/server.js
@@ -13,42 +13,66 @@ const pool = new Pool({
 });
 
 
-const trace_link = '<li><a href="trace/%1$d">Trace: %1$d</a></li>';
+const trace_link = '<li><a href="traces/%1$d">Trace: %1$d</a></li>';
 
+/*
+ * Index page
+ */
 app.get('/', function(req, res) {
-    var body = '<ul>';
+    console.log('index');
+
+    var body = '<html><head><title>MicroTrace</title></head>';
+    body += '<body><h1>MicroTrace traces</h1>';
+    body += '<ul>';
+
     pool.query('SELECT id FROM traces', (err, response) => {
         response.rows.forEach(function(row) {
             const id = row.id;
             body += sprintf(trace_link, id);
         });
         body += '</ul';
+        body += '</body></html>'
         res.send(body);
     });
 });
 
+const LEVEL_DEPTH = 5;
+
 function traverse(body, span, depth) {
-    body += Array(depth).join(' ');
-    body += 'Call at ' + new Date(span.time * 1000) + '\n';
-    body += Array(depth).join(' ');
-    body += 'from: ' + span.client + ', to: ' + span.server + '\n';
+    body += Array(depth).join('&nbsp');
+    body += 'Call at ' + new Date(span.time * 1000) + '<br>';
+    body += Array(depth).join('&nbsp');
+    body += 'from: ' + span.client + ', to: ' + span.server + '<br>';
+    body += '<hr>';
+
 
     for (var i = 0; i < span.callees.length; ++i) {
-        body = traverse(body, span.callees[i], depth + 2);
+        body = traverse(body, span.callees[i], depth + LEVEL_DEPTH);
     }
     return body;
 };
 
+/*
+ * Trace view page
+ */
 app.get('/traces/:traceId', function(req, res) {
     const traceId = req.params['traceId'];
+    console.log('/traces/' + traceId);
+
+    var body = '<html><head><title>Trace #' + traceId + '</title></head>';
+    body += '<body>';
+    body += '<h1>Trace #' + traceId + '</h1>';
+
     pool.query(
         'SELECT * FROM traces WHERE id = $1', [traceId], (err, response) => {
             const start_span = response.rows[0].body.start;
-            var body = '';
             body = traverse(body, start_span, 0);
+
+            body += '</body></html>';
             res.send(body);
         });
 });
 
 app.listen(
     3000, function() { console.log('Example app listening on port 3000!') });
+
