1055272d7249a225148f6e0bd6d89fb923eb365a
Adam Ubuntu <balogh.adam@icloud.com>
2017-08-04 12:38:16 +0100

    db scripts

--- processor/db.sql
+++ processor/db.sql
@@ -1,2 +1,14 @@
 -- traces table
-create table traces(trace_id text primary key, body json, num_spans int, duration float, ts timestamp default now());
+create table traces(
+    trace_id text primary key,
+    body json,
+    num_spans int,
+    duration float,
+    ts timestamp default now()
+);
+
+-- spans table
+create table spans(
+    id int serial,
+    json bodya -- TODO this should not be called json, its protobuf
+);
